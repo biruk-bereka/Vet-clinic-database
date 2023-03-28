f1e37d3624d8f6237cb3822c947a9c29c01cc397
biruk-bereka <birukbeletech1212@gmail.com>
2023-03-28 15:35:37 +0300

    Insert data in to animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', 'FEB-03-2020', 10.23, true, 0);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', 'NOV-15-2018', 8, true, 2);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', 'JAN-07-2021', 15.04, false, 1);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES  (4, 'Devimon', 'MAY-12-2017', 11, true, 5);
