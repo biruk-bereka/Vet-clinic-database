a79817bfd52022fa905b1685af6fd052a6125474
biruk-bereka <birukbeletech1212@gmail.com>
2023-03-28 15:19:19 +0300

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
+-- create animals table
 CREATE TABLE animals (
-    name varchar(100)
+    id int primary key not null,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempts int,
+    neutered boolean,
+    weight_kg decimal
 );
